24f8e75c6770573dda46d2105fe7785fe8cf613a
ritsu77 <honeybitterchip@gmail.com>
2024-09-02 18:24:17 +0900

    honeybitterchip.html에 내용 추가

--- dango/src/main/java/com/dangochat/dango/controller/HomeController.java
+++ dango/src/main/java/com/dangochat/dango/controller/HomeController.java
@@ -9,23 +9,17 @@ public class HomeController {
     public String home() {
         return "home";              
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     @GetMapping("/hyeonmin")
     public String hyeonmin() {
         return "hyeonmin";
     }
     
-=======
-    
     
     @GetMapping("leean")
     public String test() {
     	return"leean";
     }
->>>>>>> bba1cc6e4e7c07f356b8faca53203b6927a4cc18
-=======
     @GetMapping("honeybitterchip")
     public String honey() {
     	return "honeybitterchip";
@@ -37,5 +31,4 @@ public class HomeController {
         return "miynnn";
     }
 
->>>>>>> 0410472aa74a43a2db53ddcafef4de3e9bd2acac
 }
