78701e7c0e2520fb071c4be72c65facb478758cf
psjoo7 <jmin9480@naver.com>
2024-10-05 04:00:54 +0900

    studyRepository 대문자 소문자로 바꿈

--- dango/src/main/java/com/dangochat/dango/repository/StudyRepository.java
+++ dango/src/main/java/com/dangochat/dango/repository/StudyRepository.java
@@ -17,11 +17,11 @@ public interface StudyRepository extends JpaRepository<StudyEntity, Integer> {
 
     // [ 학습하기 ]
     // Native Query로 레벨을 기준으로 학습 콘텐츠를 랜덤하게 가져오고, LIMIT을 적용
-    @Query(value = "SELECT * FROM Study_Content WHERE level = :level AND study_content_type = :type ORDER BY RAND() LIMIT :limit", nativeQuery = true)
+    @Query(value = "SELECT * FROM study_content WHERE level = :level AND study_content_type = :type ORDER BY RAND() LIMIT :limit", nativeQuery = true)
     List<StudyEntity> findRandomByLevelAndType(@Param("level") String level, @Param("type") String type, @Param("limit") int limit);
 
     // Native Query로 특정 유저의 오답 노트(해결되지 않은)에서 studyContentId에 해당하는 StudyEntity를 랜덤하게 가져오고, LIMIT 적용
-    @Query(value = "SELECT s.* FROM Study_Content s JOIN User_Mistakes m ON s.study_content_id = m.study_content_id WHERE m.user_id = :userId AND m.mistake_resolved = false AND s.study_content_type = :type ORDER BY RAND() LIMIT :limit", nativeQuery = true)
+    @Query(value = "SELECT s.* FROM study_content s JOIN user_mistakes m ON s.study_content_id = m.study_content_id WHERE m.user_id = :userId AND m.mistake_resolved = false AND s.study_content_type = :type ORDER BY RAND() LIMIT :limit", nativeQuery = true)
     List<StudyEntity> findMistakesByUserIdAndType(@Param("userId") int userId, @Param("type") String type, @Param("limit") int limit);
 
 //    //[승급 테스트] - 한 단어 가져 와서 질문 하게 끔 하는 기능을 위해  study_content_content만 랜덤으로 가져오는 쿼리 단어 24개
